a155a3f6f07a48180bcac3a507b26e3f7c956bc4
Vergne Victor <vergne.victor@gmail.com>
2015-12-21 14:24:06 +0100

    image bot

---
+++ txtimg.js
@@ -0,0 +1,12 @@
+
+function visibilite(thingId)
+{
+var targetElement;
+targetElement = document.getElementById(thingId) ;
+if (targetElement.style.display == "none")
+{
+targetElement.style.display = "" ;
+} else {
+targetElement.style.display = "none" ;
+}
+}
